a1d2eacfcd6aa1815ea6d70b4ab8d4de9a968184
tanisal <tanisal@gmail.com>
2022-05-08 18:52:04 +0300

    more functions in binaryTree file

--- BinaryTree/bineryTree.py
+++ BinaryTree/bineryTree.py
@@ -11,15 +11,24 @@ a = Node(5)
 b = Node(11)
 c = Node(4)
 d = Node(4)
-e = Node(-2)
+e = Node(2)
 f = Node(1)
-
+g= Node(19)
+j= Node(35)
+h=Node(20)
 
 a.left= b
 a.right = c
 b.left = d
 b.right = e
+e.left =j
+e.right=h 
+f.left=g
 c.left = f
+#f.right=j
+
+
+
 
 # Iterative Solution
 def depthFirstValues(root):
@@ -36,6 +45,7 @@ def depthFirstValues(root):
             stack.append(current.left)
     return values
 
+#depthFirstValues(a)
 
 
 #Recursive Solution
@@ -60,6 +70,9 @@ def breathFirstValues(root):
 
 #breathFirstValues(a)
 
+
+
+
 #Iterative sum of the tree
 def sumTree(root):
     if root is None:
@@ -79,4 +92,231 @@ def sumTree(root):
 def sumTree_rec(root):
     if root is None: return 0
     return root.val + sumTree_rec(root.left) + sumTree_rec(root.right)
-sumTree(a)
+#sumTree(a)
+
+
+# Iterative if tree includes a target
+def tree_inludes(root,target):
+    if root is None: return False
+    queue = deque([root])
+    while queue:
+        current = queue.popleft()
+        if current.val == target:
+            return True
+        if current.left:
+            queue.append(current.left)
+        if current.right:
+            queue.append(current.right)
+    return False
+
+# Recursive Solution tree includes  s target
+def  tree_includes_rec(root,target):
+    if root is None: return False
+    if root.val == target: return True
+    return tree_includes_rec(root.left,target) or tree_includes_rec(root.right,target)
+
+
+# Iterative solution for mininum value of a tree with a given root
+def tree_min_value(root):
+    min_value = float('inf')
+    queue = deque([root])
+    while queue:
+        current=queue.popleft()
+        if min_value > current.val:
+            min_value=current.val
+        if current.left:
+            queue.append(current.left)
+        if current.right:
+            queue.append(current.right)
+    return min_value
+
+
+
+# Recursive solution
+def tree_min_value_rec(root):
+    if root is None: return float('inf')
+    return min(root.val,tree_min_value_rec(root.left),tree_min_value_rec(root.right)) 
+
+   
+
+# Recursive maxSum solution
+def maxSum_rec(root):
+    """ return the maximum sum of any root to leaf path """
+    if root is None: return float("-inf")
+    if root.left is None and root.right is None: return root.val
+    return max(root.val + maxSum_rec(root.left),root.val + maxSum_rec(root.right))
+
+#Print all path to the leafs, when having the root of a tree
+def all_paths(root,paths):
+    if root is None: 
+        return False
+    paths.append(root.val)
+    
+    if all_paths(root.left,paths) and all_paths(root.right,paths):
+        return True
+    paths.pop()
+    return True
+
+def print_all_paths(root):
+    paths=[]
+    if all_paths(root,paths):
+        for i in range(len(paths)-1):
+            print(paths[i] , end="->")
+        print(paths[len(paths)-1])
+    else:
+        print('No paths')
+#print_all_paths(a)
+
+
+
+
+
+#Find a path from root to a given target recursively
+def tree_path(root,arr,target):
+
+    if root is None:
+        return False
+
+    arr.append(root.val)
+
+    if root.val  == target:
+        return True
+
+    if tree_path(root.left,arr,target) or tree_path(root.right,arr,target):
+        return True
+    arr.pop(-1)
+    return False
+
+def print_tree_path(root,target):
+    arr=[]
+    if tree_path(root,arr,target):
+        for i in range(len(arr)-1):
+            print(arr[i] ,end = "->")
+        print(arr[len(arr)-1])
+    else:
+        print("there is no Path")
+
+# Height of a tree, or  maxdepth recursively
+def height_tree_rec(root):
+    if root is None: return -1
+    return 1 + max(height_tree_rec(root.left),height_tree_rec(root.right))
+
+# Number of nodes in binary tree
+def number_nodes(root):
+    if root is None: return 0
+    return 1+number_nodes(root.left)+number_nodes(root.right)
+#number_nodes(a) 
+
+#Number of leaf nodes in binary tree
+def number_leaf_nodes(root):
+    if root is None: return 0
+    if root.left is None and root.right is None: return 1
+    return number_leaf_nodes(root.left) + number_leaf_nodes(root.right)
+#number_leaf_nodes(a)
+
+#Print elements at a given level . Bad complexity O(n2)
+def print_given_level_rec(root,level):
+    if root is None: return
+    if level ==1: 
+        print(root.val, end=" ")
+        return
+    print_given_level_rec(root.left, level-1)
+    print_given_level_rec(root.right,level-1)
+
+#print_elements_at_level(a,1)
+
+#Print all the elements at a given level of the tree, iterative
+def print_all_elements(root):
+    if root is None:
+        return
+    queue=deque([root])
+    while queue:
+        print(queue[0].val,end=" ")
+        current= queue.popleft()
+        if current.left:
+            queue.append(current.left)
+        if current.right:
+            queue.append(current.right)
+#print_all_elements(a)
+
+#Print all the elements in the tree but by level order: complexty O(n)
+def print_all_level_order(root):
+    if root is None: return
+    queue= deque([root])
+    while queue:
+        count=len(queue)
+        while count:
+            current= queue.popleft()
+            print(current.val,end=" ")
+            if current.left:
+                queue.append(current.left)
+            if current.right:
+                queue.append(current.right)
+            count-=1
+        print(" ")
+
+#print_all_level_order(a)
+
+#Printing the nodes at agiven level, solution is iterative . complexity O(n)
+def print_elements_at_given_level(root,klevel):
+    if root is None: return
+    queue = deque([root,None]) #We add the second None, to keep track when to add to the level
+    level=1
+    while queue:
+        current= queue.popleft()
+        if level == klevel and current:
+            print(current.val, end=" ")
+        if current== None:
+            if queue:
+                queue.append(None)
+            level+=1
+            if level>klevel:
+                break
+        else:
+            if current.left:
+                queue.append(current.left)
+            if current.right:
+                queue.append(current.right)
+    print(" ")
+
+#print_elements_at_given_level(a,5)
+
+
+# Reverse level order with recursion
+def reverse_level_order_rec(root):
+    if root is None: return
+    height=height_tree_rec(root)
+
+    for i in range(height+1,0,-1):
+        print_elements_at_given_level(root,i)
+
+# Reverse level order with iteration
+def reverse_level_order(root):
+    stack=deque([])
+    queue=deque([root])
+    while queue:
+        current=queue.popleft()
+        stack.appendleft(current.val)
+        if current.left:
+            queue.append(current.left)
+        if current.right:
+            queue.append(current.right)
+    return stack
+
+#Print the lesf side of the binary tree
+
+def left_side(root):
+    queue=deque([root])
+    while len(queue): 
+        n= len(queue) # Number of nodes at current level
+        for i in range(1,n+1): #Traversing through the node line
+            current = queue.popleft()
+            if i==1:
+                print(current.val, end=" ")
+            if current.left:
+                queue.append(current.left)
+            if current.right:
+                queue.append(current.right)
+left_side(a)
+
+
